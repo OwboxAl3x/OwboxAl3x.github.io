587929c3bc8465f445bd9ce53f1950cf07151b29
Alejandro García <agarciavallecillo@gmail.com>
2019-04-27 11:10:28 +0200

    Corregido error con la edad

--- js/functions.js
+++ js/functions.js
@@ -5,11 +5,10 @@ function calcularEdad(){
 
     var years = hoy.getFullYear() - nacimiento.getFullYear();
 
-    if(hoy.getMonth() < nacimiento.getMonth() && hoy.getDay() < nacimiento.getDay()){
-
+    if(hoy.getMonth() < nacimiento.getMonth()) {
         years--;
-
-    }
+    }else if (nacimiento.getMonth() == 10)
+        if (hoy.getDay() < nacimiento.getDay()) years--;
 
     document.getElementById('anios').innerHTML = years;
 
